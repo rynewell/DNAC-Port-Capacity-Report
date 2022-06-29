77a7397213edd836ad4ca2a995735cd105db80e9
rynewell <73136671+rynewell@users.noreply.github.com>
2022-06-29 16:49:13 -0500

    Add files via upload

---
+++ 00_port_capacity_util.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+from util import get_url
+import re
+import csv
+
+report_fields = ["Hostname", "IP Address", "Utilization in %", "Total ports", "Total up"]
+
+def list_network_devices():
+    return get_url("network-device")['response']
+
+def get_interfaces(id):
+   return get_url("interface/network-device/%s" % dev_id)
+
+def print_info(interfaces):
+    total_up = 0
+    total_ports = 0
+    for interface in interfaces['response']:
+        if interface['interfaceType'] == "Physical":
+            total_ports +=1
+            if interface['status'] == "up":
+                total_up+=1
+        utilization = (total_up * 100) / total_ports
+    return utilization,total_ports,total_up
+
+if __name__ == "__main__":
+    with open('interface_utilization.csv', "w") as f:
+        writer = csv.DictWriter(f,report_fields)
+        writer.writeheader()
+        response = list_network_devices()
+        for device in response:
+            dev_id = device['id']
+            mgmt_ip = device['managementIpAddress']
+            hostname = device['hostname']
+            interfaces = get_interfaces(dev_id)
+            utilization, total_ports, total_up = print_info(interfaces)
+            print(hostname,mgmt_ip,"Utilization:{utilization}%, Total ports:{total_ports}, Total up:{total_up}".
+            format(total_ports=total_ports,
+                total_up=total_up,
+                utilization=utilization ))
+            writer.writerow({"Hostname": hostname,
+                            "IP Address": mgmt_ip,
+                            "Utilization in %": '{}%'.format(utilization),
+                            "Total ports": total_ports,
+                            "Total up": total_up})
+
+
+
+
---
+++ dnac_config.py
@@ -0,0 +1,5 @@
+import os
+DNAC=os.environ.get('DNAC','10.83.132.114')
+DNAC_PORT=os.environ.get('DNAC_PORT',443)
+DNAC_USER=os.environ.get('DNAC_USER','admin')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','fsp-WWcs!')
